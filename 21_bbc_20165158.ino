67877b247de70216bf1086e7e273f15df55e9720
vocaSource <39127771+vocaSource@users.noreply.github.com>
2020-11-13 20:50:08 +0900

    Add files via upload

---
+++ 21_bbc_20165158.ino
@@ -0,0 +1,38 @@
+// Arduino pin assignment
+#include <Servo.h>
+#define PIN_SERVO 10
+#define PIN_IR A0
+
+Servo myservo;
+
+void setup() {
+  myservo.attach(PIN_SERVO); 
+  
+// initialize serial port
+  Serial.begin(9600);
+
+}
+
+float ir_distance(void){ // return value unit: mm
+  float val;
+  float volt = float(analogRead(PIN_IR));
+  val = ((6762.0/(volt-9.0))-4.0) * 10.0;
+  return val;
+}
+
+void loop() {
+  float raw_dist = ir_distance();
+  Serial.print("min:0,max:500,dist:");
+  Serial.println(raw_dist);
+//  myservo.writeMicroseconds(1400);  while(1){}
+  if(raw_dist > 255){
+    myservo.writeMicroseconds(900); //  올리기
+    delay(1000);
+  }
+  else{
+    myservo.writeMicroseconds(1400); // 수평
+    delay(1000);
+    myservo.writeMicroseconds(1800); // 내리기
+    delay(1000);
+  }
+}
